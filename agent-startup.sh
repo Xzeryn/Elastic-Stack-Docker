1ee9a66626f5b6ae1b859cb06d48b7acb04f67ce
Xzeryn <sirrous@gmail.com>
2024-06-01 23:23:51 -0400

    added fleet startup script to enable local EAR container in Kibana

---
+++ agent-startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Add Local EAR container to Elastic
+curl --cacert /certs/ca/ca.crt -u elastic:${KIBANA_FLEET_PASSWORD} -X POST "${KIBANA_HOST}/api/fleet/agent_download_sources" \
+-H "kbn-xsrf: true" \
+-H "Content-Type: application/json" \
+-d '{
+  "name": "Local EAR",
+  "host": "http://ear:9080",
+  "is_default": true
+}'
+
+# Elastic Agent Container Entrypoint
+set -eo pipefail
+exec elastic-agent container "$@"
