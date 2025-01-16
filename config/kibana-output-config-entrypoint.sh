4792d806fd37da8abad41f18f0feeade9bf7254d
xzeryn <sirrous@gmail.com>
2025-01-16 14:39:41 -0500

    removed unnecessary lines

--- config/kibana-output-config-entrypoint.sh
+++ config/kibana-output-config-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export ES_CACERT=$(cat /usr/share/kibana/config/certs/ca/ca.crt)
-#export ES_CACERT=`echo ${CACERT}  | tr '\n' "\\n"`
 export ES_CAFINGERPRINT=`grep -v ^- /usr/share/kibana/config/certs/ca/ca.crt | base64 -d | sha256sum | awk '{ print $1 }'`
 
 exec /usr/local/bin/kibana-docker
