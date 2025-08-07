f6e3d394575cac0da12563ba20a6fc8f60a52d81
xzeryn <sirrous@gmail.com>
2025-08-07 15:27:07 -0400

    updated agent and ca-trusted-fingerprint

--- config/kibana-output-config-entrypoint.sh
+++ config/kibana-output-config-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export ES_CACERT=$(cat /usr/share/kibana/config/certs/ca/ca.crt)
-export ES_CAFINGERPRINT=`grep -v ^- /usr/share/kibana/config/certs/ca/ca.crt | base64 -d | sha256sum | awk '{ print $1 }'`
+export ES_CAFINGERPRINT=`grep -v ^- /usr/share/kibana/config/certs/es01/es01.chain.pem | base64 -d | sha256sum | awk '{ print $1 }'`
 
 exec /usr/local/bin/kibana-docker
